7a0a32caf8c7d50b02842c4a2edb27d98e7b3e4e
Candy8875 <svanuipola55@gmail.com>
2024-06-08 15:53:56 +0300

    new committ

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-if (!import.meta.env.DEV) {
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    return;
+  }
+
   const { worker } = await import("./stubs/browser.js");
-  await worker.start();
+  return worker.start();
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
