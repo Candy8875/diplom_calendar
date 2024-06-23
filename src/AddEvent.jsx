8a045f258bd734c09f6b4634873eb96ed6f83bd9
Candy8875 <svanuipola55@gmail.com>
2024-06-23 18:38:17 +0300

    new commit

--- src/AddEvent.jsx
+++ src/AddEvent.jsx
@@ -4,24 +4,19 @@ import { addEvent } from "./api/api";
 import dayjs from "dayjs";
 import { v4 as uuidv4 } from "uuid";
 import { useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
 
 export default function AddEvent() {
-  const queryClient = useQueryClient();
-  const titleRef = React.useRef(null);
-  const startDateRef = React.useRef(null);
-  const endDateRef = React.useRef(null);
-  const categoryRef = React.useRef(null);
-  const descriptionRef = React.useRef(null);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  async function onClickAddEvent(close) {
-    const event = {
-      id: uuidv4(),
-      title: titleRef.current.value,
-      startDate: startDateRef.current.value,
-      endDate: endDateRef.current.value,
-      category: categoryRef.current.value,
-      description: descriptionRef.current.value,
-    };
+  const queryClient = useQueryClient();
+  async function onClickAddEvent(close, event) {
+    event.id = uuidv4();
     await addEvent(event);
 
     const formattedDate = dayjs(event.startDate).format("YYYY-MM-DD");
@@ -29,6 +24,7 @@ export default function AddEvent() {
       queryKey: ["events", formattedDate],
     });
 
+    reset();
     close();
   }
   return (
@@ -42,57 +38,178 @@ export default function AddEvent() {
         {(close) => (
           <div className="portal-window">
             <h1>Добавить событие</h1>
-            <div className="window-event">
-              <label>Название:</label>
-              <input
-                id="title"
-                ref={titleRef}
-                type="text"
-                className="input-event"
-              />
-              <label>Начало:</label>
-              <input
-                id="startDate"
-                ref={startDateRef}
-                type="datetime-local"
-                className="input-event"
-              />
-              <label>Окончание:</label>
-              <input
-                id="endDate"
-                ref={endDateRef}
-                type="datetime-local"
-                className="input-event"
-              />
-              <label>Категория:</label>
-              <select id="category" ref={categoryRef} className="input-event">
-                <option value="important" id="eventImportant">
-                  Важное событие
-                </option>
-                <option value="meeting" id="eventMeeting">
-                  Встреча с руководителем
-                </option>
-                <option value="team-meeting" id="eventTeamMeeting">
-                  Командное собрание
-                </option>
-                <option value="conference" id="eventConference">
-                  Конференция
-                </option>
-              </select>
-              <label>Описание:</label>
-              <input
-                id="description"
-                ref={descriptionRef}
-                type="text"
-                className="input-event"
-              />
-            </div>
-            <button
-              onClick={() => onClickAddEvent(close)}
-              className="button-event"
+            <form
+              onSubmit={handleSubmit((data) => {
+                console.log(errors);
+                onClickAddEvent(close, data);
+              })}
             >
-              Добавить событие
-            </button>
+              <div className="window-event">
+                <label>Название:</label>
+                <input
+                  {...register("title", { required: true })}
+                  id="title"
+                  type="text"
+                  className={`input-event ${
+                    errors.startDate ? "input-failed" : ""
+                  }`}
+                />
+                {errors.title && (
+                  <p className="font-failed">Пожалуйста, введите название</p>
+                )}
+                <label>Начало:</label>
+                <input
+                  id="startDate"
+                  {...register("startDate", {
+                    required: true,
+                    validate: {
+                      isInWorkingHours: (startDate) => {
+                        const startTime = dayjs(startDate, "HH:mm").hour(9);
+                        const endTime = dayjs(startDate, "HH:mm").hour(20);
+
+                        return (
+                          dayjs(startDate, "HH:mm").isBetween(
+                            startTime,
+                            endTime,
+                            "hour",
+                            "[)"
+                          ) ||
+                          "Время начала события должно быть в диапазоне [09:00-20:00)"
+                        );
+                      },
+                      isSameDay: (startDate, formValues) => {
+                        if (!formValues.endDate) {
+                          return true;
+                        }
+
+                        return (
+                          dayjs(startDate).isSame(
+                            dayjs(formValues.endDate),
+                            "day"
+                          ) || "Начало и конец события должны быть в один день"
+                        );
+                      },
+                      isLessThanEndDate: (startDate, formValues) => {
+                        if (!formValues.endDate) {
+                          return true;
+                        }
+
+                        return (
+                          dayjs(startDate) < dayjs(formValues.endDate) ||
+                          "Время начала события не может быть позже окончания"
+                        );
+                      },
+                    },
+                  })}
+                  type="datetime-local"
+                  className={`input-event ${
+                    errors.startDate ? "input-failed" : ""
+                  }`}
+                />
+                {errors.startDate?.type === "required" && (
+                  <p className="font-failed">Пожалуйста, введите начало</p>
+                )}
+                {(errors.startDate?.type === "isInWorkingHours" ||
+                  errors.startDate?.type === "isSameDay" ||
+                  errors.startDate?.type === "isLessThanEndDate") && (
+                  <p className="font-failed">{errors.startDate.message}</p>
+                )}
+                <label>Окончание:</label>
+                <input
+                  id="endDate"
+                  {...register("endDate", {
+                    required: true,
+                    validate: {
+                      isInWorkingHours: (value) => {
+                        const startTime = dayjs(value, "HH:mm")
+                          .hour(9)
+                          .minute(15);
+                        const endTime = dayjs(value, "HH:mm")
+                          .hour(20)
+                          .minute(0);
+
+                        return (
+                          dayjs(value, "HH:mm").isBetween(
+                            startTime,
+                            endTime,
+                            null,
+                            "[]"
+                          ) ||
+                          "Время начала события должно быть в диапазоне [09:15-20:00]"
+                        );
+                      },
+                      isSameDay: (endDate, formValues) => {
+                        if (!formValues.startDate) {
+                          return true;
+                        }
+
+                        return (
+                          dayjs(endDate).isSame(
+                            dayjs(formValues.startDate),
+                            "day"
+                          ) || "Начало и конец события должны быть в один день"
+                        );
+                      },
+                      isGreaterThanStartDate: (endDate, formValues) => {
+                        if (!formValues.startDate) {
+                          return true;
+                        }
+
+                        return (
+                          dayjs(endDate) > dayjs(formValues.startDate) ||
+                          "Время окончания события не может быть раньше начала"
+                        );
+                      },
+                    },
+                  })}
+                  type="datetime-local"
+                  className={`input-event ${
+                    errors.startDate ? "input-failed" : ""
+                  }`}
+                />
+                {errors.endDate?.type === "required" && (
+                  <p className="font-failed">Пожалуйста, введите конец</p>
+                )}
+                {(errors.endDate?.type === "isInWorkingHours" ||
+                  errors.endDate?.type === "isSameDay" ||
+                  errors.endDate?.type === "isGreaterThanStartDate") && (
+                  <p className="font-failed">{errors.endDate.message}</p>
+                )}
+                <label>Категория:</label>
+                <select
+                  id="category"
+                  {...register("category")}
+                  className="input-event"
+                >
+                  <option value="important" id="eventImportant">
+                    Важное событие
+                  </option>
+                  <option value="meeting" id="eventMeeting">
+                    Встреча с руководителем
+                  </option>
+                  <option value="team-meeting" id="eventTeamMeeting">
+                    Командное собрание
+                  </option>
+                  <option value="conference" id="eventConference">
+                    Конференция
+                  </option>
+                </select>
+                <label>Описание:</label>
+                <input
+                  id="description"
+                  {...register("description")}
+                  type="text"
+                  className={`input-event ${
+                    errors.startDate ? "input-failed" : ""
+                  }`}
+                />
+                <input
+                  type="submit"
+                  value="Добавить событие"
+                  className="button-event"
+                />
+              </div>
+            </form>
           </div>
         )}
       </Popup>
--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,14 @@
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
 export const getEventsByDate = async (date) => {
-  const response = await fetch(`/getEventsByDate?date=${date}`, {
+  const response = await fetch(`${apiBaseUrl}/getEventsByDate?date=${date}`, {
     method: "GET",
   });
 
   return await response.json();
 };
 export const addEvent = async (event) => {
-  const response = await fetch(`/addEvent`, {
+  const response = await fetch(`${apiBaseUrl}/addEvent`, {
     method: "POST",
     body: JSON.stringify(event),
   });
